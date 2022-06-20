fa7c476f7065e861fa23f8c236e19f799bc9a2e8
Jack Peeples <jack.peeples@PM-MAC-1320.local>
2022-06-20 18:09:46 +0200

    group id filter

--- internal/controller/LocationController.go
+++ internal/controller/LocationController.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"LocationLunatics/internal/models"
 	"LocationLunatics/internal/repository"
+	"fmt"
+	"strconv"
 )
 
 type LocationController struct {
@@ -13,4 +15,24 @@ func NewLocationController() (lc *LocationController) {
 }
 func (lc *LocationController) GetLocations(bb models.BoundingBox) (locations []models.Location, err error) {
 	return repository.GetAllLocationData(), nil
+}
+
+func (lc *LocationController) GetGroupLocations(groupId string) (locations []models.Location, err error) {
+	groupIdInt, err := strconv.Atoi(groupId)
+	if err != nil {
+		return locations, err
+	}
+	allLocations := repository.GetAllLocationData()
+
+	finalLocations := []models.Location{}
+	for _, loc := range allLocations {
+		if loc.ZoneGroupID == groupIdInt {
+			finalLocations = append(finalLocations, loc)
+			fmt.Println("Succeed: " + loc.PMZoneCode)
+		} else {
+			fmt.Println("FAIL: " + loc.PMZoneCode)
+		}
+	}
+
+	return finalLocations, nil
 }
